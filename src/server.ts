bc636c69cce2800807c120b585c63dbe94f2ba24
zeroxiru <zerox.iru@gmail.com>
2025-06-29 14:53:13 +0600

    changing the paasword

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { Server} from "http";
 import app from './app'
 import mongoose from 'mongoose';
+import dotenv from "dotenv"
+dotenv.config();
 let server: Server;
 const PORT = 7000;
 async function main() {
    try {
-    await mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.fmsye.mongodb.net/library-mgt-app?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fmsye.mongodb.net/library-mgt-app?retryWrites=true&w=majority&appName=Cluster0`);
     console.log("Connected to MongoDB using Mongoose");
     server = app.listen(PORT, ()=> { 
         console.log(`App is listening on port ${PORT}`);
